409cf5deb24ec223dac29aff53c6215bfe8ef9c9
Everton Cordeiro <99029999+EvertonCordeiro1994@users.noreply.github.com>
2022-04-04 09:37:03 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+<script>
+    var numero = document.getElementById('resultado').innerHTML;
+    function insert(num){
+        var numero = document.getElementById('resultado').innerHTML;
+        document.getElementById('resultado').innerHTML = numero + num;
+        }
+
+    function limpar(){
+        document.getElementById('resultado').innerHTML = " ";
+    }
+    function bs(){
+        var resultado = document.getElementById('resultado').innerHTML;
+        document.getElementById('resultado').innerHTML = resultado.substring(0,resultado.length-1);
+    }
+   
+    function calcular(){
+        var resultado = document.getElementById('resultado').innerHTML;
+        document.getElementById('resultado').innerHTML= eval(resultado);
+    }
+</script>
